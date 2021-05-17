01d79d45e6f11dadc0b7d416bf5af540ba569124
AnnaMeller97 <anna.czapiewska.97@gmail.com>
2021-05-17 18:12:23 +0200

    Init repository

---
+++ src/main/java/empik/user/rate/EmpikRateApplication.java
@@ -0,0 +1,14 @@
+package empik.user.rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpikRateApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/empik/user/rate/EmpikRateApplicationTests.java
@@ -0,0 +1,13 @@
+package empik.user.rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
